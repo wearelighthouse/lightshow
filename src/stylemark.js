55b0b7c3e98b68f7d2fb78dbde5b6d36e66576da
burntcustard <burntcustard@gmail.com>
2019-06-21 16:26:32 +0100

    Minor text fixes

--- src/stylemark.js
+++ src/stylemark.js
@@ -18,9 +18,10 @@ var defaultMatchExtensions = new RegExp(`\\.(${extensions.join('|')})$`);
 var defaultExcludeDirectories = ['.git', 'node_modules'];
 var defaultNamesFormat = (name) => {
 	return name
+	  .replace('/(README)|(LICENSE)/i', '')
 	  .replace(/[_-]/g, ' ')
 		.split(' ')
-		.map(w =>  w.substring(0,1).toUpperCase()+ w.substring(1))
+		.map(w =>  w.substring(0, 1).toUpperCase() + w.substring(1))
 		.join(' ');
 }
 
